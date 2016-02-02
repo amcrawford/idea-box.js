11a9bf682547aa98e8dc480e361642913e154df0
Amber Crawford <ammcrawford@gmail.com>
2016-02-02 10:56:54 -0700

    Resolve issue with multiple up/downvotes * Can now successfully upvote multiple times without refresh * Page order issue back

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -3,47 +3,49 @@ $(document).ready(function(){
   createIdea()
 });
 
-function likeIdea(id, oldQuality){
+function likeIdea(id){
   $('#thumb-up' + id).on('click', function(){
+    $.ajax($.getJSON('/api/v1/ideas/'+ id, function(idea){
+      var newQuality = function(){
+        if (idea.quality === 'swill'){
+          return 'plausible'
+        } else { return 'genius'}
+      };
 
-    var newQuality = function(){
-      if (oldQuality === 'swill'){
-        return 'plausible'
-      } else { return 'genius'}
-    };
-
-    $.ajax({
-      type: 'PUT',
-      url: '/api/v1/ideas/' + id + '.json',
-      data: {
-        idea: {quality: newQuality}
-      },
-      success: function(){
-        $('#idea-quality' + id).html(newQuality);
-      }
-    })
+      $.ajax({
+        type: 'PUT',
+        url: '/api/v1/ideas/' + id + '.json',
+        data: {
+          idea: {quality: newQuality}
+        },
+        success: function(idea){
+          $('#idea-quality' + id).html(newQuality);
+        }
+      })
+    }))
   })
 };
 
-function dislikeIdea(id, oldQuality){
+function dislikeIdea(id){
   $('#thumb-down' + id).on('click', function(){
+    $.ajax($.getJSON('/api/v1/ideas/'+ id, function(idea){
+      var newQuality = function(){
+        if (idea.quality === 'genius'){
+          return 'plausible'
+        } else { return 'swill'}
+      };
 
-    var newQuality = function(){
-      if (oldQuality === 'genius'){
-        return 'plausible'
-      } else { return 'swill'}
-    };
-
-    $.ajax({
-      type: 'PUT',
-      url: '/api/v1/ideas/' + id + '.json',
-      data: {
-        idea: {quality: newQuality}
-      },
-      success: function(){
-        $('#idea-quality' + id).html(newQuality);
-      }
-    })
+      $.ajax({
+        type: 'PUT',
+        url: '/api/v1/ideas/' + id + '.json',
+        data: {
+          idea: {quality: newQuality}
+        },
+        success: function(idea){
+          $('#idea-quality' + id).html(newQuality);
+        }
+      })
+    }))
   })
 };
 
@@ -83,15 +85,15 @@ function renderIdea(idea){
   var body = truncateBody(idea.body);
 
   $('#idea-index').prepend(
-    '<p><h4>' + idea.id + '. ' + idea.title + '</h4>' +
+    '<p class="idea"><h4>' + idea.id + '. ' + idea.title + '</h4>' +
     '"' + body + '" <br><br>' +
     '<strong> People think this idea is: </strong><em><div id="idea-quality' + idea.id + '">' + idea.quality +
     '</div></em></p>' +
     '<a href="#"><i class="material-icons" id="thumb-up' + idea.id + '">thumb_up</i></a>' +
     '<a href="#"><i class="material-icons" id="thumb-down'+ idea.id + '">thumb_down</i></a>'
   );
-  likeIdea(idea.id, idea.quality);
-  dislikeIdea(idea.id, idea.quality);
+  likeIdea(idea.id);
+  dislikeIdea(idea.id);
 };
 
 function truncateBody(body){
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -9,6 +9,10 @@ class Api::V1::IdeasController < Api::V1::BaseController
     respond_with Idea.create(idea_params), location: nil
   end
 
+  def show
+    respond_with Idea.find(params["id"])
+  end
+
   def update
     @idea = Idea.find(params["id"]).update(idea_params)
     respond_with @idea, json: @idea
