057d70938fd445b667f7bdf336b19d39ec3fcb32
Amber Crawford <ammcrawford@gmail.com>
2016-02-03 14:29:24 -0700

    Model and api controller tests passing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'minitest/pride'
+require 'webmock'
+require 'vcr'
+require "simplecov"
+require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  VCR.configure do |config|
+    config.allow_http_connections_when_no_cassette = true
+    config.cassette_library_dir = "test/cassettes"
+    config.hook_into :webmock
+  end
+
+  def json_response
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 end
