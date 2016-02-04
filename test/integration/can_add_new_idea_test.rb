c445c68ee1490e535f629552d1f0b5ed4b57ee8d
Amber Crawford <ammcrawford@gmail.com>
2016-02-04 08:48:12 -0700

    Add testing for truncate method

--- test/integration/can_add_new_idea_test.rb
+++ test/integration/can_add_new_idea_test.rb
@@ -18,4 +18,20 @@ class AddNewIdeasTest < ActionDispatch::IntegrationTest
       assert page.has_content?("Totally works.")
     end
   end
+
+  test '#idea created with body longer than 100 chars will be truncated' do
+    visit '/'
+
+    assert page.has_content?("Have an Idea?")
+
+    within "#new-idea" do
+      fill_in "New Title", with: "Create New Idea"
+      fill_in "New Description", with: "This is so many characters long but you
+      will never see the last word because my program totally rocks!"
+      click_button "Save"
+    end
+
+    assert page.has_content?("This is so many characters")
+    refute page.has_content?("totally rocks!")
+  end
 end
