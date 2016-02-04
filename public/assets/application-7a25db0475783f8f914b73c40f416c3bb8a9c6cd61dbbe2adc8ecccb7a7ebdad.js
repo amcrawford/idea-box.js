433686013a60623b3cd5bba182c6b1eec8157940
Amber Crawford <ammcrawford@gmail.com>
2016-02-03 17:56:50 -0700

    Clobber assets for heroku

--- public/assets/application-7a25db0475783f8f914b73c40f416c3bb8a9c6cd61dbbe2adc8ecccb7a7ebdad.js
+++ public/assets/application-7a25db0475783f8f914b73c40f416c3bb8a9c6cd61dbbe2adc8ecccb7a7ebdad.js
@@ -18455,13 +18455,35 @@ Picker.extend( 'pickadate', DatePicker )
 $(document).ready(function(){
   getIdeaIndex()
   createIdea()
+  searchIdeas()
 });
+
+function sortIdeas(){
+  var qualityMap = {
+    "genius": 2,
+    "plausible": 1,
+    "swill": 0
+  };
+
+  var ideas = $('#idea-index div.idea');
+
+  var sortedIdeas = ideas.sort(function(a, b){
+    return qualityMap[$(a).find('.quality').text()] > qualityMap[$(b).find('.quality').text()];
+  });
+
+
+  $('#sort-index').on('click', function(){
+    $('#idea-index').html(sortedIdeas);
+  }).on('click', function(){
+    $('#idea-index').html(sortedIdeas.reverse());
+  })
+};
 
 function likeIdea(id){
   $('#thumb-up' + id).on('click', function(){
     event.preventDefault();
 
-    $.ajax($.getJSON('/api/v1/ideas/'+ id, function(idea){
+    $.getJSON('/api/v1/ideas/'+ id, function(idea){
       var newQuality = function(){
         if (idea.quality === 'swill'){
           return 'plausible'
@@ -18478,7 +18500,7 @@ function likeIdea(id){
           $('#idea-quality' + id).html(newQuality);
         }
       })
-    }))
+    })
   })
 };
 
@@ -18486,7 +18508,7 @@ function dislikeIdea(id){
   $('#thumb-down' + id).on('click', function(){
     event.preventDefault();
 
-    $.ajax($.getJSON('/api/v1/ideas/'+ id, function(idea){
+    $.getJSON('/api/v1/ideas/'+ id, function(idea){
       var newQuality = function(){
         if (idea.quality === 'genius'){
           return 'plausible'
@@ -18503,7 +18525,7 @@ function dislikeIdea(id){
           $('#idea-quality' + id).html(newQuality);
         }
       })
-    }))
+    })
   })
 };
 
@@ -18515,9 +18537,50 @@ function getIdeaIndex(){
   })
 };
 
-function editIdea(id){
+function editIdeaTitle(id){
+  $('#idea-title' + id).keydown(function(){
+    if (event.keyCode === 13) {
+      saveEditedTitle(id, $('#idea-title' + id).text())
+    }
+  })
+};
+
+function saveEditedTitle(id, ideaTitle){
+  $.ajax({
+    type: 'PUT',
+    url: '/api/v1/ideas/' + id + '.json',
+    data: {idea: {
+      title: ideaTitle }},
+    success: function(){
+      $('#idea-title' + id).html(ideaTitle);
+    }
+  });
+};
+
+function editIdeaBody(id){
+  $('#idea-body' + id).keydown(function(){
+    if (event.keyCode === 13) {
+      saveEditedBody(id, $('#idea-body' + id).text())
+    }
+  })
+};
+
+function saveEditedBody(id, ideaBody){
+  $.ajax({
+    type: 'PUT',
+    url: '/api/v1/ideas/' + id + '.json',
+    data: {idea: {
+      body: ideaBody }},
+    success: function(){
+      $('#idea-body' + id).html(ideaBody);
+    }
+  });
+};
+
+function editFullIdea(id){
   $('#edit-idea-button' + id).on('click', function(){
     $('#edit-idea' + id).show();
+
     $('#edit-idea-button' + id).hide();
 
     $('#save-edit' + id).on('click', function(){
@@ -18545,11 +18608,8 @@ function editIdea(id){
 };
 
 function closeEditFields(id){
-  $('#edit-idea-title' + id).hide();
-  $('#edit-idea-body' + id).hide();
+  $('#edit-idea' + id).hide();
   $('#edit-idea-button' + id).show();
-  $('#save-edit' + id).hide()
-  $('#cancel-edit' + id).hide()
 };
 
 function cancelEdit(id){
@@ -18581,34 +18641,65 @@ function createIdea(){
   });
 };
 
+function deleteIdea(id){
+  $('#delete-idea-button' + id).on('click', function(){
+    $.ajax({
+      type: 'DELETE',
+      url: '/api/v1/ideas/' + id + '.json',
+      success: function(){
+        $('#idea-' + id).remove();
+      }
+    });
+  })
+};
+
+function searchIdeas(){
+  $("#search-text").keyup(function(){
+		var filter = $(this).val();
+		$("#idea-index").children().each(function(){
+			if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+				$(this).fadeOut();
+			} else {
+				$(this).show();
+			}
+		});
+	});
+};
+
 function renderIdea(idea){
   var body = truncateBody(idea.body);
 
   $('#idea-index').prepend(
-    '<p class="idea"><h4>' + idea.id + '. <span id="idea-title' + idea.id + '">' + idea.title + '</span></h4>' +
-    '<span id="idea-body'+idea.id+'">"' + body + '" </span><br><br>' +
-    '<strong> People think this idea is: </strong><em><div id="idea-quality' + idea.id + '">' + idea.quality +
-    '</div></em></p>' +
-    '<a href="#"><i class="material-icons" id="thumb-up' + idea.id + '">thumb_up</i></a>' +
-    '<a href="#"><i class="material-icons" id="thumb-down'+ idea.id + '">thumb_down</i></a><br>' +
-    '<div id="edit-idea'+ idea.id +'"><div id="edit-idea-title' + idea.id + '"><input class="validate" type="text" id="edit-idea-title-text' + idea.id + '" value="New Title Here"></div>' +
-    '<div id="edit-idea-body' + idea.id +'"><input class="validate" type="text" id="edit-idea-body-text' + idea.id + '" value="New Description Here"></div>' +
-    '<input class="btn-small pull-right" id="save-edit'+ idea.id +'" type="button" name="submit" value="Save">' +
-    '<input class="btn-small pull-right" id="cancel-edit'+ idea.id +'" type="button" name="submit" value="Cancel"></div>' +
-    '<input class="btn-small pull-right" id="edit-idea-button'+ idea.id +'" type="button" name="submit" value="Edit">'
+    '<div class="idea" id="idea-' + idea.id + '"><p class="idea"><h4>' + idea.id + '. <span id="idea-title"><span contentEditable=true id="idea-title' + idea.id + '">' + idea.title + '</span> </span>  ' +
+    '<a href="#"><i class="material-icons teal-text" id="thumb-up' + idea.id + '">thumb_up</i></a> ' +
+    '<a href="#"><i class="material-icons teal-text" id="thumb-down'+ idea.id + '">thumb_down</i></a><br>' +
+    '</h4>' +
+    '<span id="idea-body"><span id="idea-body'+idea.id+'" contentEditable=true>"' + body + '" </span></span><br><br>' +
+    '<strong> People think this idea is: </strong><em><span class="quality" id="idea-quality' + idea.id + '">' + idea.quality +
+    '</span></em></p>' +
+    '<div id="edit-idea'+ idea.id +'"><div id="edit-idea-title' + idea.id + '"><input class="validate" type="text" id="edit-idea-title-text' + idea.id + '" placeholder="New Title Here"></div>' +
+    '<div id="edit-idea-body' + idea.id +'"><input class="validate" type="text" id="edit-idea-body-text' + idea.id + '" placeholder="New Description Here"></div>' +
+    '<input class="btn btn-small pull-right" id="save-edit'+ idea.id +'" type="button" name="submit" value="Save"> ' +
+    '<input class="btn btn-small pull-right" id="cancel-edit'+ idea.id +'" type="button" name="submit" value="Cancel"></div><br> ' +
+    '<input class="btn btn-small pull-right" id="edit-idea-button'+ idea.id +'" type="button" name="submit" value="Edit">  ' +
+    '<input class="btn btn-small pull-right" id="delete-idea-button'+ idea.id +'" type="button" name="submit" value="Delete"></div><br>'
 
   );
   $('#edit-idea' + idea.id).hide();
 
-  editIdea(idea.id);
+  editFullIdea(idea.id);
+  editIdeaTitle(idea.id);
+  editIdeaBody(idea.id);
   likeIdea(idea.id);
   dislikeIdea(idea.id);
+  deleteIdea(idea.id);
+  sortIdeas();
 };
 
 function truncateBody(body){
   if (body.length > 100){
-    var trimmedBody = body.substring(0,101);
-    return trimmedBody.substring(0, Math.min(trimmedBody.length, trimmedBody.lastIndexOf(' ')))
+    var trimmedBody = body.substring(0,98);
+    return trimmedBody.substring(0, Math.min(trimmedBody.length, trimmedBody.lastIndexOf(' '))) + '...'
   } else {
     return body
   };
